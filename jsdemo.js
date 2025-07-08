ebec075ce6ca1daf70b9a470aad267ecf556c2b9
why-ally <nicholas.3changes@gmail.com>
2025-07-08 16:12:37 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -1,4 +1,4 @@
 console.log("This output is from the external JS file");
-concole.log("Name: Nicholas johnson");
+console.log("Name: Nicholas johnson");
 console.log("School: CPCC Merancas");
 console.log("Grade: Rising Senior");
