c37169b260591063a2a75967bebf6cb5d7d6bbcf
why-ally <nicholas.3changes@gmail.com>
2025-07-08 16:42:53 -0400

    Update jsdemo.js

--- jsdemo.js
+++ jsdemo.js
@@ -2,3 +2,11 @@ console.log("This output is from the external JS file");
 console.log("Name: Nicholas johnson");
 console.log("School: CPCC Merancas");
 console.log("Grade: Rising Senior");
+
+var name = "Nicholas";
+let school = "Hopewell/CPCC Merancas Campas";
+var grade = "11th";
+
+document.getElementById("name").innerHTML = "Name: <b>" + name + "</b>";
+document.getElementById("grade").innerHTML = "Grade: <b>" + Grade + "</b>";
+document.getElementById("school").innerHTML = "School: <b>" + School + "</b>";
