e78544834ad54f6bcb4b5c6da7131dd3055bdc6e
why-ally <nicholas.3changes@gmail.com>
2025-07-10 16:52:49 -0400

    Update jsreview.js

--- jsreview.js
+++ jsreview.js
@@ -2,3 +2,19 @@ var course = "Technology";
 let myName = "Nicholas";
 
 console.log(`This page was created in the STEM PREP ${course} by ${myName}`);
+
+alert(`This page was created in the STEM PREP ${course} by ${myName}`);
+
+function output()
+{
+  let name = document.getElementById("name").value;
+   var school = document.getElementById("school").value;
+  let grade = document.getElementById("grade").value;
+  
+
+let output = "Name: <b>" + name + "</b><br>";
+output += "grade: <b>" + grade + "<b><br>";
+  output += "school: <b>" + school + "</b><br>";
+
+  document.getElementById("output").innerHTML = output;
+}
