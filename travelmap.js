4aea86aa4ccf77046d70a7a4d726a8d1b402e783
why-ally <nicholas.3changes@gmail.com>
2025-07-17 16:46:57 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -10,3 +10,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let circleMarker = L.circle(coordinates, {
+  color: "blue",
+  fillColor: "red",
+  fillOpacity: 0.25,
+  radius: 50
+}).bindPopup(`<iframe width="280" height="160" src="https://www.youtube.com/embed/TVYP644I-O8?si=eMNOK5BuYrYVUUBr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`).addTo(map);
