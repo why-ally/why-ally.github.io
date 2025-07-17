4f751223dcafca9e332833d0a9972c93a2265c28
why-ally <nicholas.3changes@gmail.com>
2025-07-17 16:05:24 -0400

    Create travelmap.js

---
+++ travelmap.js
@@ -0,0 +1,12 @@
+//35.34183° N, 80.83392° W
+var coordinates = [35.34183, -80.83392}];
+
+let map = L.map("map", {
+  center: coordinates,
+  zoom:10
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
